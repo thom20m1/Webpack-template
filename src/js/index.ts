02471c41f837d4131046b9e097bf4092f814fcf2
Thomas Fogh <thom20m1@edu.easj.dk>
2019-10-01 21:40:28 +0200

    Changes to Index.ts and Index.htm

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,35 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("input");
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let operationElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("operations");
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let outputElement: HTMLDivElement = <HTMLDivElement>document.getElementById("output");
+
+let operateButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("operate");
+operateButton.addEventListener("click", ()=>
+{
+    let resultstring: string="";
+    outputElement.style.fontStyle="normal";
+    
+    let operation: string = operationElement.value;
+    if(operation==="upperCase")
+    {
+        resultstring=inputElement.value.toUpperCase();
+    }    
+    else if(operation==="lowerCase")
+    {
+        resultstring=inputElement.value.toLowerCase();
+    }
+    else if(operation==="italics")
+    {
+        resultstring=inputElement.value;
+        outputElement.style.fontStyle="italic";
+    }
+    else
+    {
+        resultstring=inputElement.value.replace("o", "jens-hansen havde en bondegård");
+        resultstring=inputElement.value.replace("O", "jens-hansen havde en bondegård");
+    }
+    outputElement.innerHTML=resultstring;
+})
+
+
